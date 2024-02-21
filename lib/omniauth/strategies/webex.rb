b32b0f1c98fba7a8c8b1d90a651dc1339dcae95c
Ed Umansky <edumansky@gmail.com>
2024-02-20 22:40:32 -0700

    Ability to override any authorize_params passed as request params

--- lib/omniauth/strategies/webex.rb
+++ lib/omniauth/strategies/webex.rb
@@ -2,12 +2,22 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Webex < OmniAuth::Strategies::OAuth2
+      AUTHORIZE_OPTIONS = %i[response_type max_age login_hint prompt scope state redirect_uri nonce]
       option :name, 'webex'
 
       option :client_options, {
         authorize_url: 'https://webexapis.com/v1/authorize',
         token_url: 'https://webexapis.com/v1/access_token'
       }
+
+      # Override any authorize_params passed as request params
+      def authorize_params
+        super.tap do |params|
+          (options[:authorize_options] & options[:overridable_authorize_options]).each do |k|
+            params[k] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
+          end
+        end
+      end
 
       uid { raw_info['id'] }
 
